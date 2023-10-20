44fa73f8edd968b06eaa979a028dd8266905ad9c
LebgaLesly <lebgalesly1@gmail.com>
2023-10-20 16:19:17 -0700

    technet backend

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const mongoose = require('mongoose')
 const cors = require('cors');
 const productRoutes = require('./Routes/ProductsRoutes')
 const userRoutes = require('./Routes/UserRoutes')
+const corsOptions = {
+  origin: 'https://master.d2sb0ghkw4k0bw.amplifyapp.com/',
+  optionsSuccessStatus: 200 // For legacy browser support
+}
 
 // init app & middleware
 const app = express();
 app.use(express.json());
-app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-
-}));
+app.use(cors(corsOptions));
 
 //connecting to database
 mongoose.connect(process.env.URI).then(() => {

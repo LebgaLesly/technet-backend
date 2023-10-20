a7a07e3737542be41b0634540f014ff4623e7138
LebgaLesly <lebgalesly1@gmail.com>
2023-10-20 16:10:39 -0700

    technet backend

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ const userRoutes = require('./Routes/UserRoutes')
 // init app & middleware
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+
+}));
 
 //connecting to database
 mongoose.connect(process.env.URI).then(() => {
